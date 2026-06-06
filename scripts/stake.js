d7ff2f7e3d1812d19a99af6e94c85964e85c8192
rumus-music <t3guhwahono@gmail.com>
2026-06-06 12:00:53 +0700

    Add staking script for TronWeb integration

---
+++ scripts/stake.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const TronWeb = require("tronweb");
+
+const tronWeb = new TronWeb({
+  fullHost: process.env.FULL_HOST,
+  privateKey: process.env.PRIVATE_KEY
+});
+
+async function main() {
+
+  const staking =
+    await tronWeb.contract()
+      .at(process.env.STAKING_ADDRESS);
+
+  const tx =
+    await staking
+      .stake(
+        "100000000000000000000"
+      )
+      .send();
+
+  console.log(tx);
+}
+
+main();
