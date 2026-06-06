d6b3e9b3eea1bb48641fc7ec244156917f08b103
rumus-music <t3guhwahono@gmail.com>
2026-06-06 20:21:24 +0700

    Create config.php

---
+++ faucet/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'rpc_url' => 'https://go.getblock.io/gb_645ca8a90df2934c481eb679/',
+    
+    // PRIVATE KEY WALLET FAUCET
+    'private_key' => 'gb_645ca8a90df2934c481eb679',
+
+    // Wallet faucet
+    'faucet_address' => '0x579a78B234E9b26922ce7a7fFb6d1aacd65db5bc',
+
+    // Jumlah ETH yang dikirim
+    'amount_eth' => '0.001'
+];
