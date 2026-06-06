31d52c6278395f262e619f0064b4b4ef2599df95
rumus-music <t3guhwahono@gmail.com>
2026-06-06 20:22:07 +0700

    Create index.php

---
+++ faucet/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Base Sepolia Faucet</title>
+</head>
+<body>
+
+<h2>Base Sepolia Faucet</h2>
+
+<form action="send.php" method="POST">
+    <input
+        type="text"
+        name="address"
+        placeholder="0x..."
+        required
+        style="width:400px;"
+    >
+    <button type="submit">Claim</button>
+</form>
+
+</body>
+</html>
